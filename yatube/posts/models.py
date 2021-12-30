7fa2ef8d3b2ec3e460f97eefbcbc346f5ba6b479
Ilnur Arifullin <ChloePrice34@yandex.ru>
2021-12-30 21:10:21 +0300

    PEP8_changes

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -32,6 +32,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text[15]
-    
+
     class Meta:
         ordering = ('-pub_date',)
+    
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Group, Post
 
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context) 
-    
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
@@ -18,7 +19,8 @@ def group_posts(request, slug):
     }
     return render(request, 'posts/group_list.html', context)
 
+
 context = {
-    "title" : "Это главная страница проекта Yatube",
-    "posts_title" : "Здесь будет информация о группах проекта Yatube",
-    }
+    "title": "Это главная страница проекта Yatube",
+    "posts_title": "Здесь будет информация о группах проекта Yatube",
+          }
